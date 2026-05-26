25dc64390acbfd34d4052bbfbc231e825c7b8016
hafsahrm <mangorangca.hafsah@gmail.com>
2026-05-26 21:11:56 +0800

    removed harcoded defaults

--- src/app.module.ts
+++ src/app.module.ts
@@ -39,9 +39,9 @@ import {
         type: 'postgres',
         host: config.get<string>('DB_HOST', 'localhost'),
         port: config.get<number>('DB_PORT', 5432),
-        username: config.get<string>('DB_USERNAME', 'postgres'),
-        password: config.get<string>('DB_PASSWORD', 'postgres'),
-        database: config.get<string>('DB_NAME', 'project_nexus'),
+        username: config.get<string>('DB_USERNAME', 'root'),
+        password: config.get<string>('DB_PASSWORD', 'rootpass'),
+        database: config.get<string>('DB_NAME', 'project-nexus-db'),
         entities: [
           User,
           Stage,
